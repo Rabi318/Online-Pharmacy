d397a34dbd9f1542f6e1e40db0094a72e2b0ada7
RabinarayanS <rabinarayansahoo9348@gamil.com>
2025-04-23 08:12:42 +0530

    landing page

--- index.js
+++ index.js
@@ -4,3 +4,24 @@ const navLinks = document.querySelector(".nav-right ul");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginLink = document.getElementById("loginLink");
+  const loginModal = document.getElementById("loginModal");
+  const closeModal = document.getElementById("closeModal");
+
+  loginLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    loginModal.style.display = "block";
+  });
+
+  closeModal.addEventListener("click", (e) => {
+    e.preventDefault();
+    loginModal.style.display = "none";
+  });
+  window.addEventListener("click", (e) => {
+    if (e.target === loginModal) {
+      loginModal.style.display = "none";
+    }
+  });
+});
